1d2bb72422e964cff1abfa46a2692b5fad3c0968
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-08 14:26:29 +0100

    :heavy_plus_sign: 5-add-proto add protobuf and grpc dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,11 @@ buildscript {
         set("junit_version", "4.13.2")
         set("android_junit_version", "1.1.4")
         set("espresso_core_version", "3.5.0")
+        set("nav_version", "2.5.2")
+        set("gson_version", "2.10.1")
+        set("protobuf_version", "3.21.12")
+        set("grpc_version", "1.51.1")
+        set("grpc_kt_version", "1.3.0")
     }
 }
 
