3231814e0fd725ef276f69fb7166a151982ae242
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-08 17:12:31 +0100

    :card_file_box: 10-add-room chore[Database|Module] add PopMesDatabase.kt and its dependencies

--- app/src/main/java/fr/messager/popmes/di/DatabaseModule.kt
+++ app/src/main/java/fr/messager/popmes/di/DatabaseModule.kt
@@ -1,10 +1,25 @@
 package fr.messager.popmes.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import fr.messager.popmes.data.database.PopMesDatabase
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun providePopMesDatabase(
+        @ApplicationContext context: Context,
+    ) = Room.databaseBuilder(
+        context = context,
+        klass = PopMesDatabase::class.java,
+        name = "pop_mes_database",
+    ).build()
 }
