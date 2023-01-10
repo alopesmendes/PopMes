607264e73518091a9c5924bd337f30747fc189ac
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-10 11:55:26 +0100

    :lipstick: 20-ui-window-sizes chore[Components] add PopMesWindowSize.kt

---
+++ app/src/main/java/fr/messager/popmes/presentation/components/dimensions/WindowSizeDimension.kt
@@ -0,0 +1,6 @@
+package fr.messager.popmes.presentation.components.dimensions
+
+sealed class WindowSizeDimension {
+    object Width: WindowSizeDimension()
+    object Height: WindowSizeDimension()
+}
