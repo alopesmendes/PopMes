3f1b4ee157ba95c0fdabe76d07ef79e727ab44b8
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-11 17:57:41 +0100

    19-navigation-ui chore[Navigation] create NavItem.kt that will serve to build elements for nav bar

---
+++ app/src/main/java/fr/messager/popmes/presentation/navigation/NavItem.kt
@@ -0,0 +1,59 @@
+package fr.messager.popmes.presentation.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+import fr.messager.popmes.R
+
+
+sealed class NavItem(
+    @DrawableRes private val imageId: Int? = null,
+    private val imageVector: ImageVector? = null,
+    private val imageBitmap: ImageBitmap? = null,
+    @StringRes val titleId: Int,
+    val screen: Screen,
+    val contentDescription: String? = null,
+) {
+
+    @Throws(IllegalArgumentException::class)
+    @Composable
+    fun painterIcon(): Painter {
+        return if (imageId != null)
+            painterResource(imageId)
+        else if (imageVector != null)
+            rememberVectorPainter(imageVector)
+        else if (imageBitmap != null)
+            BitmapPainter(imageBitmap)
+        else
+            throw IllegalArgumentException("All fields are null in NavItem")
+    }
+
+    object Home: NavItem(
+        titleId = R.string.nav_item_title_home,
+        imageVector = Icons.Filled.Home,
+        screen = Screen.Home,
+        contentDescription = "home nav item",
+    )
+
+    object Contacts: NavItem(
+        titleId = R.string.nav_item_title_contacts,
+        imageId = R.drawable.ic_contacts,
+        screen = Screen.Contacts,
+        contentDescription = "contacts nav item",
+    )
+
+    object Tasks: NavItem(
+        titleId = R.string.nav_item_title_tasks,
+        imageId = R.drawable.ic_task,
+        screen = Screen.Tasks,
+        contentDescription = "tasks nav item",
+    )
+}
