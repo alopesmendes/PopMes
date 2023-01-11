e33b0158299764d8269558ac62d5cf43c98357d6
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-11 18:05:17 +0100

    :lipstick: 19-navigation-ui chore[Extension] update navigation methods in Extension.kt

--- app/src/main/java/fr/messager/popmes/common/Extension.kt
+++ app/src/main/java/fr/messager/popmes/common/Extension.kt
@@ -1,103 +1,149 @@
 package fr.messager.popmes.common
 
-import androidx.navigation.NavController
+import android.app.Activity
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import fr.messager.popmes.common.Constants.gson
+import fr.messager.popmes.presentation.navigation.NavItem
 import fr.messager.popmes.presentation.navigation.Screen
 import fr.messager.popmes.presentation.navigation.arguments.NavData
 import fr.messager.popmes.presentation.screen.AddGroupScreen
 import fr.messager.popmes.presentation.screen.AddUserScreen
 import fr.messager.popmes.presentation.screen.ContactsScreen
+import fr.messager.popmes.presentation.screen.ConversationScreen
 import fr.messager.popmes.presentation.screen.FileGeneratorScreen
 import fr.messager.popmes.presentation.screen.TasksElementScreen
 import fr.messager.popmes.presentation.screen.TasksScreen
+import kotlinx.coroutines.CoroutineScope
 
 object Extension {
-    inline fun <reified V: NavData> V.mapNavDataToJson(): String {
+    inline fun <reified V : NavData> V.mapNavDataToJson(): String {
         return gson.toJson(this)
     }
 
-    inline fun <reified V: NavData> String?.mapToNavData(): V? {
+    inline fun <reified V : NavData> String?.mapToNavData(): V? {
         if (this.isNullOrBlank())
             return null
         return gson.fromJson(this, V::class.java)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     fun NavGraphBuilder.contactsNavigation(
-        navController: NavController,
+        activity: Activity,
+        navItems: List<NavItem>,
+        drawerState: DrawerState,
+        scope: CoroutineScope,
+        selectedItem: Int,
+        onSelectedItemChange: (Int) -> Unit,
+        onNavigate: (String) -> Unit,
     ) {
-        navigation(
-            startDestination = Screen.Contacts.route(),
+        composable(
             route = Screen.Contacts.route(),
+            arguments = Screen.Contacts.navParams(),
         ) {
-            composable(
-                route = Screen.Contacts.route(),
-                arguments = Screen.Contacts.navParams(),
-            ) {
-                ContactsScreen(navController = navController)
-            }
+            ContactsScreen(
+                activity = activity,
+                navItems = navItems,
+                onNavigate = onNavigate,
+                drawerState = drawerState,
+                scope = scope,
+                selectedItem = selectedItem,
+                onSelectedItemChange = onSelectedItemChange,
+            )
+        }
 
-            composable(
-                route = Screen.AddGroup.route(),
-                arguments = Screen.AddGroup.navParams(),
-            ) {
-                AddGroupScreen(navController = navController,)
-            }
+        composable(
+            route = Screen.AddGroup.route(),
+            arguments = Screen.AddGroup.navParams(),
+        ) {
+            AddGroupScreen(
+                activity = activity,
+                onNavigate = onNavigate,
+            )
+        }
 
-            composable(
-                route = Screen.AddUser.route(),
-                arguments = Screen.AddUser.navParams(),
-            ) {
-                AddUserScreen(navController = navController)
-            }
+        composable(
+            route = Screen.AddUser.route(),
+            arguments = Screen.AddUser.navParams(),
+        ) {
+            AddUserScreen(
+                activity = activity,
+                onNavigate = onNavigate,
+            )
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     fun NavGraphBuilder.conversationNavigation(
-        navController: NavController
+        activity: Activity,
+        navItems: List<NavItem>,
+        drawerState: DrawerState,
+        scope: CoroutineScope,
+        selectedItem: Int,
+        onSelectedItemChange: (Int) -> Unit,
+        onNavigate: (String) -> Unit,
     ) {
-        navigation(
-            startDestination = Screen.Conversation.route(),
+        composable(
             route = Screen.Conversation.route(),
+            arguments = Screen.Conversation.navParams(),
         ) {
-            composable(
-                route = Screen.Conversation.route(),
-                arguments = Screen.Conversation.navParams(),
-            ) {
-                ContactsScreen(navController = navController)
-            }
+            ConversationScreen(
+                activity = activity,
+                navItems = navItems,
+                onNavigate = onNavigate,
+                drawerState = drawerState,
+                scope = scope,
+                selectedItem = selectedItem,
+                onSelectedItemChange = onSelectedItemChange,
+            )
+        }
 
-            composable(
-                route = Screen.FileGenerator.route(),
-                arguments = Screen.FileGenerator.navParams(),
-            ) {
-                FileGeneratorScreen(navController = navController)
-            }
+        composable(
+            route = Screen.FileGenerator.route(),
+            arguments = Screen.FileGenerator.navParams(),
+        ) {
+            FileGeneratorScreen(
+                activity = activity,
+                onNavigate = onNavigate,
+            )
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     fun NavGraphBuilder.tasksNavigation(
-        navController: NavController
+        activity: Activity,
+        navItems: List<NavItem>,
+        drawerState: DrawerState,
+        scope: CoroutineScope,
+        selectedItem: Int,
+        onSelectedItemChange: (Int) -> Unit,
+        onNavigate: (String) -> Unit,
     ) {
-        navigation(
-            startDestination = Screen.Tasks.route(),
+        composable(
             route = Screen.Tasks.route(),
+            arguments = Screen.Tasks.navParams(),
         ) {
-            composable(
-                route = Screen.Tasks.route(),
-                arguments = Screen.Tasks.navParams(),
-            ) {
-                TasksScreen(navController = navController)
-            }
+            TasksScreen(
+                activity = activity,
+                navItems = navItems,
+                onNavigate = onNavigate,
+                drawerState = drawerState,
+                scope = scope,
+                selectedItem = selectedItem,
+                onSelectedItemChange = onSelectedItemChange,
+            )
+        }
 
-            composable(
-                route = Screen.TasksElement.route(),
-                arguments = Screen.TasksElement.navParams(),
-            ) {
-                TasksElementScreen(navController = navController)
-            }
+        composable(
+            route = Screen.TasksElement.route(),
+            arguments = Screen.TasksElement.navParams(),
+        ) {
+            TasksElementScreen(
+                activity = activity,
+                onNavigate = onNavigate,
+            )
         }
     }
 }
