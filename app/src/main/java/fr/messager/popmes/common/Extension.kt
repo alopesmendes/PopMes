ca179b9ea4240b241f90ec8bf566fdef23578ff0
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-17 11:58:02 +0100

    29-navigation-window-sizes chore[Extension] update methods to navigate + add method to convert byte array to hex string and vice versa

--- app/src/main/java/fr/messager/popmes/common/Extension.kt
+++ app/src/main/java/fr/messager/popmes/common/Extension.kt
@@ -2,14 +2,16 @@ package fr.messager.popmes.common
 
 import android.app.Activity
 import android.text.format.DateUtils
+import android.util.Log
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import fr.messager.popmes.common.Constants.gson
+import com.google.protobuf.MessageLite
 import fr.messager.popmes.domain.model.contact.User
-import fr.messager.popmes.domain.model.message.Message
-import fr.messager.popmes.domain.model.message.MessageType
+import fr.messager.popmes.mapper.ConversationParamsToConversationParamsProto.reverseMapTo
 import fr.messager.popmes.presentation.navigation.NavItem
 import fr.messager.popmes.presentation.navigation.Screen
 import fr.messager.popmes.presentation.navigation.arguments.NavData
@@ -20,18 +22,17 @@ import fr.messager.popmes.presentation.screen.ConversationScreen
 import fr.messager.popmes.presentation.screen.FileGeneratorScreen
 import fr.messager.popmes.presentation.screen.TasksElementScreen
 import fr.messager.popmes.presentation.screen.TasksScreen
+import fr.messager.popmes.proto.ConversationParamsProto
 import kotlinx.coroutines.CoroutineScope
 import java.time.Instant
 
 object Extension {
-    inline fun <reified V : NavData> V.mapNavDataToJson(): String {
-        return gson.toJson(this)
-    }
 
-    inline fun <reified V : NavData> String?.mapToNavData(): V? {
-        if (this.isNullOrBlank())
-            return null
-        return gson.fromJson(this, V::class.java)
+    private inline fun <reified V : NavData, reified T : MessageLite> String?.mapToNavData(
+        parseFrom: (ByteArray?) -> T,
+        mapTo: T.() -> V,
+    ): V {
+         return parseFrom(this?.hexStringToByteArray()).mapTo()
     }
 
     @OptIn(ExperimentalMaterial3Api::class)
@@ -43,6 +44,7 @@ object Extension {
         selectedItem: Int,
         onSelectedItemChange: (Int) -> Unit,
         onNavigate: (String) -> Unit,
+        onBack: () -> Unit,
     ) {
         composable(
             route = Screen.Contacts.route(),
@@ -56,6 +58,7 @@ object Extension {
                 scope = scope,
                 selectedItem = selectedItem,
                 onSelectedItemChange = onSelectedItemChange,
+                onBack = onBack,
             )
         }
 
@@ -80,6 +83,8 @@ object Extension {
         }
     }
 
+    private val TAG = Extension::class.simpleName
+
     @OptIn(ExperimentalMaterial3Api::class)
     fun NavGraphBuilder.conversationNavigation(
         activity: Activity,
@@ -87,25 +92,21 @@ object Extension {
         drawerState: DrawerState,
         scope: CoroutineScope,
         selectedItem: Int,
+        currentUser: User,
         onSelectedItemChange: (Int) -> Unit,
         onNavigate: (String) -> Unit,
+        onBack: () -> Unit,
     ) {
         composable(
             route = Screen.Conversation.route(),
             arguments = Screen.Conversation.navParams(),
         ) {
-            val currentUser = User(
-                id = "0",
-                firstName = "Ailton",
-                lastName = "Lopes Mendes",
-                phoneNumber = "+33781831024",
+            val params = it.arguments?.getString(Constants.PARAM_CONVERSATION).mapToNavData(
+                parseFrom = ConversationParamsProto::parseFrom,
+                mapTo = { this.reverseMapTo() },
             )
-            val manuel = User(
-                id = "1",
-                firstName = "Manuel",
-                lastName = "Lopes Mendes",
-                phoneNumber = "+33681831024",
-            )
+            Log.d(TAG, "conversationNavigation: ${params.messages.size}")
+
             ConversationScreen(
                 activity = activity,
                 navItems = navItems,
@@ -115,36 +116,8 @@ object Extension {
                 selectedItem = selectedItem,
                 onSelectedItemChange = onSelectedItemChange,
                 currentUser = currentUser,
-                messages = listOf(
-                    Message(
-                        id = "0",
-                        messageType = MessageType.Message,
-                        from = currentUser,
-                        to = manuel,
-                        date = Instant.now(),
-                    ),
-                    Message(
-                        id = "1",
-                        messageType = MessageType.Message,
-                        from = manuel,
-                        to = currentUser,
-                        date = Instant.now(),
-                    ),
-                    Message(
-                        id = "2",
-                        messageType = MessageType.Message,
-                        from = manuel,
-                        to = currentUser,
-                        date = Instant.now(),
-                    ),
-                    Message(
-                        id = "3",
-                        messageType = MessageType.Message,
-                        from = currentUser,
-                        to = manuel,
-                        date = Instant.now(),
-                    )
-                )
+                messages = params.messages,
+                onBack = onBack,
             )
         }
 
@@ -168,6 +141,7 @@ object Extension {
         selectedItem: Int,
         onSelectedItemChange: (Int) -> Unit,
         onNavigate: (String) -> Unit,
+        onBack: () -> Unit,
     ) {
         composable(
             route = Screen.Tasks.route(),
@@ -181,6 +155,7 @@ object Extension {
                 scope = scope,
                 selectedItem = selectedItem,
                 onSelectedItemChange = onSelectedItemChange,
+                onBack = onBack,
             )
         }
 
@@ -205,4 +180,23 @@ object Extension {
             DateUtils.FORMAT_ABBREV_RELATIVE,
         )?.toString() ?: ""
     }
+
+    fun NavHostController.navigateTo(destination: String) {
+        navigate(destination) {
+            // Pop up to the start destination of the graph to
+            // avoid building up a large stack of destinations
+            // on the back stack as users select items
+            popUpTo(graph.findStartDestination().id) {
+                saveState = true
+            }
+            // Avoid multiple copies of the same destination when
+            // reselecting the same item
+            launchSingleTop = true
+            // Restore state when reselecting a previously selected item
+            restoreState = true
+        }
+    }
+
+    fun ByteArray.toHex() = this.joinToString(separator = "") { it.toInt().and(0xff).toString(16).padStart(2, '0') }
+    private fun String.hexStringToByteArray() = ByteArray(this.length / 2) { this.substring(it * 2, it * 2 + 2).toInt(16).toByte() }
 }
