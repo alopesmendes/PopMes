703745be37d4b5e964d89d572e4b88bea7437bd5
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-17 11:40:30 +0100

    :truck: 29-navigation-window-sizes chore[Constants] rename PARAM_GROUP_CHAT to PARAM_CONVERSATION

--- app/src/main/java/fr/messager/popmes/common/Constants.kt
+++ app/src/main/java/fr/messager/popmes/common/Constants.kt
@@ -1,15 +1,11 @@
 package fr.messager.popmes.common
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import java.time.Instant
-
 object Constants {
-    val gson: Gson = GsonBuilder()
-        .registerTypeAdapter(Instant::class.java, DateDeserializer())
-        .setPrettyPrinting()
-        .serializeNulls()
-        .create()
+//    val gson: Gson = GsonBuilder()
+//        .registerTypeAdapter(Instant::class.java, DateDeserializer())
+//        .setPrettyPrinting()
+//        .serializeNulls()
+//        .create()
 
-    const val PARAM_GROUP_CHAT = "group_chat"
+    const val PARAM_CONVERSATION = "conversation"
 }
