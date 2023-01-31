c027674c59d042da902b1397aa7b4201f5f72ef7
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-01-31 09:48:53 +0100

    :recycle: 37-create-task-elements-components refactor TaskPriority.kt and TaskType.kt

--- app/src/main/java/fr/messager/popmes/domain/model/task/TaskPriority.kt
+++ app/src/main/java/fr/messager/popmes/domain/model/task/TaskPriority.kt
@@ -1,8 +1,29 @@
 package fr.messager.popmes.domain.model.task
 
-enum class TaskPriority {
-    LOW,
-    MEDIUM,
-    HIGH,
-    URGENT,
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import fr.messager.popmes.R
+
+enum class TaskPriority(
+    @StringRes val stringId: Int,
+    @DrawableRes val drawableId: Int,
+) {
+    LOW(
+        stringId = R.string.low,
+        drawableId = R.drawable.ic_low_priority,
+    ),
+    HIGH(
+        stringId = R.string.high,
+        drawableId = R.drawable.ic_priority_high,
+    ),
+    URGENT(
+        stringId = R.string.urgent,
+        drawableId = R.drawable.ic_warning,
+    ),
 }
+
+val priorities = listOf(
+    TaskPriority.LOW,
+    TaskPriority.HIGH,
+    TaskPriority.URGENT,
+)
