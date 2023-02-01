5d930ebf38d23e5af82843e57cd117184ef7a411
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-02-01 19:24:46 +0100

    17-entities-and-dao chore[Converter] add converting methods for task and message

--- app/src/main/java/fr/messager/popmes/data/database/converters/ProtoDataConverter.kt
+++ app/src/main/java/fr/messager/popmes/data/database/converters/ProtoDataConverter.kt
@@ -2,8 +2,14 @@ package fr.messager.popmes.data.database.converters
 
 import androidx.room.TypeConverter
 import fr.messager.popmes.domain.model.contact.Contact
+import fr.messager.popmes.domain.model.message.Message
+import fr.messager.popmes.domain.model.task.Task
 import fr.messager.popmes.mapper.ContactToContactProto.mapTo
+import fr.messager.popmes.mapper.MessageToMessageProto.reverseMapTo
+import fr.messager.popmes.mapper.TaskToTaskProto.reverseMapTo
 import fr.messager.popmes.proto.ContactProto
+import fr.messager.popmes.proto.MessageProto
+import fr.messager.popmes.proto.TaskProto
 
 class ProtoDataConverter {
 
@@ -15,6 +21,28 @@ class ProtoDataConverter {
 
     @TypeConverter
     fun fromContactToByteArray(value: Contact?): ByteArray? {
+        return value?.toByteArray()
+    }
+
+    @TypeConverter
+    fun fromByteArrayToMessage(value: ByteArray?): Message? {
+        val proto = MessageProto.parseFrom(value)
+        return proto?.reverseMapTo()
+    }
+
+    @TypeConverter
+    fun fromMessageToByteArray(value: Message?): ByteArray? {
+        return value?.toByteArray()
+    }
+
+    @TypeConverter
+    fun fromByteArrayToTask(value: ByteArray?): Task? {
+        val proto = TaskProto.parseFrom(value)
+        return proto?.reverseMapTo()
+    }
+
+    @TypeConverter
+    fun fromMessageToByteArray(value: Task?): ByteArray? {
         return value?.toByteArray()
     }
 }
