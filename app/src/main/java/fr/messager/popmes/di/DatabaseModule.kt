a70307ee172254a8584ef28ffd9b40caba427c25
alopesmendes <ailtonlopesmendes9@gmail.com>
2023-02-01 17:45:44 +0100

    17-entities-and-dao chore[Module] update DatabaseModule.kt and RepositoryModule.kt b adding contact dependencies

--- app/src/main/java/fr/messager/popmes/di/DatabaseModule.kt
+++ app/src/main/java/fr/messager/popmes/di/DatabaseModule.kt
@@ -1,7 +1,6 @@
 package fr.messager.popmes.di
 
 import android.content.Context
-import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,9 +16,11 @@ object DatabaseModule {
     @Singleton
     fun providePopMesDatabase(
         @ApplicationContext context: Context,
-    ) = Room.databaseBuilder(
-        context,
-        PopMesDatabase::class.java,
-        "pop_mes_database.db",
-    ).build()
+    ): PopMesDatabase {
+        return PopMesDatabase.getInstance(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserDao(database: PopMesDatabase) = database.contactDao()
 }
--- app/src/main/java/fr/messager/popmes/di/RepositoryModule.kt
+++ app/src/main/java/fr/messager/popmes/di/RepositoryModule.kt
@@ -1,10 +1,20 @@
 package fr.messager.popmes.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import fr.messager.popmes.data.database.dao.ContactDao
+import fr.messager.popmes.data.repository.ContactRepositoryImpl
+import fr.messager.popmes.domain.repository.ContactRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
+    @Provides
+    fun provideContactRepository(
+        contactDao: ContactDao,
+    ): ContactRepository = ContactRepositoryImpl(
+        contactDao = contactDao,
+    )
 }
